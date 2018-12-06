a7ad5177dff2add4f59cf6f248cf6d4ea0d2486c
bularond <bularond@gmail.com>
2018-12-06 18:44:15 +0500

    Start project

---
+++ telegram/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import telebot
+
+bot = telebot.TeleBot("714110486:AAG6qYzhscu2yFy9cDBylNVJSUymC96A040")
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    print(message.text)
+    bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+
+
+bot.polling(none_stop = True, interval = 0)
+
+# Обработчик команд '/start' и '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    pass
+
+ # Обработчик для документов и аудиофайлов
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_document_audio(message):
+    pass
+
+bot.polling(none_stop=True, interval=0)
