b1c3d398b1d5f28e0b262ef45a08d13e7f7859a9
bularond <bularond@gmail.com>
2019-04-03 21:00:34 +0500

    Alfa version. Ready to deploy

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -6,21 +6,16 @@ import logging
 import telebot
 from time import sleep
 
-# Каждый раз получаем по 10 последних записей со стены
 USER_API_URL = "https://godville.net/gods/api/bularond/dce5b193b3da"
 
 BOT_TOKEN = '878053869:AAG96xIVJAdYVajscg_RZQc4giVBqEFufIQ'
-CHANNEL_NAME = '@godvile_winner223'
+CHAT_ID = 388003444
 
-# Если True, предполагается использование cron для запуска скрипта
-# Если False, процесс запускается и постоянно висит запущенный
 SINGLE_RUN = False
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-true, false, null = True, False, None
-
-old_data, now_data = {}, {"name":"Winner223","godname":"Bularond","gender":"male","level":45,"max_health":276,"inventory_max_num":29,"motto":"Физмат - сила","clan":"Блоги StopGame","clan_position":"советник","alignment":"нейтральный","bricks_cnt":897,"pet":{"pet_name":"Пинки «Пропащий» ❌","pet_class":"огнелис","pet_level":10,"wounded":true},"ark_completed_at":null,"arena_won":33,"arena_lost":28,"health":173,"quest_progress":2,"exp_progress":84,"godpower":77,"gold_approx":"около 3 тысяч","diary_last":"✑ Разобрал монстра на мозаику. За творческий труд забрал 38 золотых.","town_name":"","distance":4,"arena_fight":false,"inventory_num":2,"quest":"пустить по ветру летучий корабль","activatables":[]}
+old_data, now_data = {}, {}
 
 def update_data():
     global now_data, old_data
@@ -44,20 +39,13 @@ def gen_new_messeges():
 
 def send_messages(messeges):
     for messege in messeges:
-        bot.send_message(CHANNEL_NAME, messege)
+        bot.send_message(CHAT_ID, messege)
     time.sleep(1)
 
 if __name__ == '__main__':
-    # Избавляемся от спама в логах от библиотеки requests
-    logging.getLogger('requests').setLevel(logging.CRITICAL)
-    # Настраиваем наш логгер
-    logging.basicConfig(format='[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s', level=logging.INFO,
-                        filename='bot_log.log', datefmt='%d.%m.%Y %H:%M:%S')
+    update_data()
     while True:
         update_data()
         send_messages(gen_new_messeges())
-        # Пауза в 4 минуты перед повторной проверкой
-        logging.info('[App] Script went to sleep.')
         time.sleep(60)
 
-    logging.info('[App] Script exited.\n')
